1f1954d8758d643423a87651e598bb40b3965686
hpacco <himanshu.puri@accolitedigital.com>
2024-02-28 11:56:18 +0530

    first commit

---
+++ src/main/java/com/accolitedigital/checksetup/ChecksetupApplication.java
@@ -0,0 +1,14 @@
+package com.accolitedigital.checksetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecksetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChecksetupApplication.class, args);
+		System.out.println("Program ran successfully");
+	}
+
+}
---
+++ src/test/java/com/accolitedigital/checksetup/ChecksetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accolitedigital.checksetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChecksetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
