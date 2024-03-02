1c2776b4f229c3f59726aba5e519b7d5c38b78b1
htrepo <himanshutecha@gmail.com>
2024-03-02 12:32:41 +0530

    fixes

--- src/test/java/com/accolitedigital/checksetup/ChecksetupApplicationTests.java
+++ src/test/java/com/accolitedigital/checksetup/ChecksetupApplicationTests.java
@@ -1,9 +1,7 @@
 package com.accolitedigital.checksetup;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ChecksetupApplicationTests {
 
 	@Test
